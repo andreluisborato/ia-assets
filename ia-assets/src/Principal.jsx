3daea5cf46e9a98af587b33e00e49bc8f42cdbe1
andreluisborato <andre.borato.ferreira@escola.pr.gov.br>
2026-08-06 14:45:25 +0000

    2

--- ia-assets/src/Principal.jsx
+++ ia-assets/src/Principal.jsx
@@ -7,10 +7,22 @@ import './Principal.css'
 function Principal() {
   const [count, setCount] = useState(0)
 
+function nome() {
+  let nome = document.getElementById("msg")
+  const [texto, setTexto] = useState(nome)
+
+  const alterarNome = () => {
+    setTexto(nome)
+  }
+}
+
   return (
     <>
-      <h1>Olá</h1>
-      <textarea name="msg" id="msg" className="msg" placeholder="Descreva seu assets e deixe a ia criar: "></textarea>
+      <div className="container">
+        <h1 className="txt"></h1>
+        <button>1</button>
+        <textarea name="msg" id="msg" className="msg" placeholder="Descreva seu asset e deixe a ia criar: "></textarea>
+      </div>
     </>
   )
 }
