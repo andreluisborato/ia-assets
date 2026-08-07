c2612f54fbbf154b99dd9cdaa7536a1ba6970c94
andreluisborato <andre.borato.ferreira@escola.pr.gov.br>
2026-08-07 11:02:10 +0000

    3

--- ia-assets/src/Principal.jsx
+++ ia-assets/src/Principal.jsx
@@ -5,26 +5,26 @@ import heroImg from './assets/hero.png'
 import './Principal.css'
 
 function Principal() {
-  const [count, setCount] = useState(0)
 
-function nome() {
-  let nome = document.getElementById("msg")
-  const [texto, setTexto] = useState(nome)
+  const [texto, setTexto] = useState('');
 
-  const alterarNome = () => {
-    setTexto(nome)
+  function lidarComClique() {
+    const nome = "André";
+    setTexto(`Olá, ${nome}`);
   }
-}
 
   return (
     <>
       <div className="container">
-        <h1 className="txt"></h1>
-        <button>1</button>
+
+         {texto && <h1 className="txt animar-texto">{texto}</h1>}
+        
+        <button id="btn" onClick={lidarComClique}>1</button>
+        
         <textarea name="msg" id="msg" className="msg" placeholder="Descreva seu asset e deixe a ia criar: "></textarea>
       </div>
     </>
   )
-}
+} 
 
 export default Principal
